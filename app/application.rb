fb32c98f8a9090b2756924a198044c5b252d6395
chic-page-6502 <zacharytq@gmail.com>
2020-01-24 00:59:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     if Time.now.to_i < 12
       resp.write "Good Morning!"
     else
-      rest.write "Good Afternoon!"
+      resp.write "Good Afternoon!"
     end
     resp.finish
   end
