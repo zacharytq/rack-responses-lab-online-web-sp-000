d790126dc7bcfe2f137d91e5736858ae65a46476
chic-page-6502 <zacharytq@gmail.com>
2020-01-24 01:04:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if Time.now.to_i >= 12
+    if Time.now.hour.to_i >= 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
